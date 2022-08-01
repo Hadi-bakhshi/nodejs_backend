126610908c2edaa2e2a81263af0bb93796c0f825
Hadi <hadibakhshi277@gmail.com>
2022-08-01 17:50:04 +0430

    for reza

--- src/server.ts
+++ src/server.ts
@@ -17,20 +17,21 @@ app.use(cors());
 
 app.use('/api/user', router);
 
-app.post('/myuserishere', async (req, res) => {
-  const user = await prisma.$queryRaw`INSERT INTO [dbo].[Spy]
-  ([Name])
-VALUES
-  (N'${req.body.userData.fullName}')`;
-  console.log(user);
-});
+// app.post('/myuserishere', async (req, res) => {
+//   console.log(req.body);
+//   //   const user = await prisma.$queryRaw`INSERT INTO [dbo].[Spy]
+//   //   ([Name])
+//   // VALUES
+//   //   (N'${req.body.userData}')`;
+//   // console.log(user);
+// });
 
-app.get('/test', async (req, res) => {
-  console.log('this is test');
-  res.json({
-    name: 'reza',
-  });
-});
+// app.get('/test', async (req, res) => {
+//   console.log('this is test');
+//   res.json({
+//     name: 'reza',
+//   });
+// });
 
 app.listen(port, () => {
   return console.log(`Express is listening at ${port}`);
