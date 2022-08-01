839c126ebeabd8003069721850d66cffb83d295d
Hadi <hadibakhshi277@gmail.com>
2022-08-01 17:55:30 +0430

    db changed

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -12,19 +12,28 @@ const prisma = new PrismaClient();
 router.post(
   '/login',
   asyncHandler(async (req: Request | any, res: Response | any) => {
+    // const { error } = loginValidation(req.body);
+    // if (error) return res.status(400).json({ message: 'اوضاع خیته' });
+    // const reqUser = req.body.username;
+    // const reqPass = req.body.password;
+    // const user = await prisma.user.findUnique({
+    //   where: {
+    //     username: reqUser,
+    //   },
+    // });
+    // console.log(user);
+    // if (!user)
+    //   return res.status(400).json({ message: 'کاربر مورد نظر وجود ندارد' });
+    // const validPass = (await user.password) === reqPass;
+    // if (!validPass)
     const { error } = loginValidation(req.body);
     if (error) return res.status(400).json({ message: 'اوضاع خیته' });
     const reqUser = req.body.username;
-    const reqPass = req.body.password;
-    const user = await prisma.user.findUnique({
-      where: {
-        username: reqUser,
-      },
-    });
+    const user = data.users.find((u) => u.username === reqUser);
     console.log(user);
     if (!user)
       return res.status(400).json({ message: 'کاربر مورد نظر وجود ندارد' });
-    const validPass = (await user.password) === reqPass;
+    const validPass = await bcrypt.compare(req.body.password, user.password);
     if (!validPass)
       return res.status(400).json({ message: 'رمز عبور نادرست می باشد' });
     res.send({
